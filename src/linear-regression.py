a9586616e886e04aae0f44f61fdcadfc77a5d701
Lakith <lakith.hos@outlook.com>
2019-01-16 16:31:22 +0530

    Create linear-regression.py

---
+++ src/linear-regression.py
@@ -0,0 +1,8 @@
+
+import db-sqlite3 # learn more: https://python.org/pypi/db-sqlite3
+import pyplot as plt # learn more: https://python.org/pypi/pyplot
+import pandas as pd # learn more: https://python.org/pypi/pandas
+import numpy as np # learn more: https://python.org/pypi/numpy
+
+data = pd.read_csv(path, header=None,names=['Population','Profit'])
+
